6cbb1e752945788405f0812f88339c849c6363f5
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-08-15 08:58:00 -0400

    update version number

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_packages(where='.', lib_prefix='', exclude=()):
 
 setup(
     name='mo-testing',
-    version="1.0.17168",
+    version="1.0.17227",
     description='More Testing! Extends the `unittest.TestCase` to provide deep, yet fuzzy, structural comparisons',
     long_description=long_desc,
     author='Kyle Lahnakoski',
