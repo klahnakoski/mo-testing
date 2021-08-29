7e36eaa1562b486e4ae33085f2d2e70b9a5614ce
Kyle Lahnakoski <kyle@lahnakoski.com>
2021-08-29 15:53:31 -0400

    update version number

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ setup(
     description='More Testing! Extends the `unittest.TestCase` to provide deep, yet fuzzy, structural comparisons',
     extras_require={"tests":[]},
     include_package_data=True,
-    install_requires=["mo-collections==5.45.21241","mo-dots==5.22.21182","mo-future==5.17.21182","mo-logs==5.44.21241","mo-math==5.23.21182","mo-threads==5.37.21239"],
+    install_requires=["mo-collections==5.45.21241","mo-dots==5.22.21182","mo-future==5.17.21182","mo-logs==5.44.21241","mo-math==5.53.21241","mo-threads==5.53.21241"],
     license='MPL 2.0',
     long_description='# More Testing\n\n`FuzzyTestCase` extends the `unittest.TestCase` to provide deep, yet fuzzy, structural comparisons; intended for use in test cases dealing with JSON.\n\n\n## Details\n\nThe primary method is the `assertEqual` method with the following parameters:\n\n* `test_value` - the value, or structure being tested\n* `expected` - the expected value or structure.  In the case of a number, the accuracy is controlled by the following parameters.  In the case of a structure, only the not-null parameters of `expected` are tested for existence.\n* `msg` - Detailed error message if there is no match\n* `digits` - number of decimal places of accuracy required to consider two values equal\n* `places` - number of significant digits used to compare values for accuracy\n* `delta` - maximum difference between values for them to be equal\n\nThis method `assertEqual` is recursive; it does a deep comparison; it can not handle cycles in the data structure.\n\n\n',
     long_description_content_type='text/markdown',
     name='mo-testing',
     packages=["mo_testing"],
     url='https://github.com/klahnakoski/mo-testing',
-    version='5.49.21241'
+    version='5.53.21241'
 )
